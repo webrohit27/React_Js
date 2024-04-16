21d5849833bde052f54f0a7e4cefc821d82c802b
rohitrodge <webrohit27@gmail.com>
2024-04-16 11:50:38 +0530

    update App

--- webpages/src/App.jsx
+++ webpages/src/App.jsx
@@ -1,9 +1,18 @@
 import Navbar from './components/Navbar';
 import './App.css';
 import Sidebar from './components/Sidebar';
+import { useState } from 'react';
 
 function App(){
+
+let [name, setName] = useState()
+
+let name = "Rohit";
+function handleClick(){
+ 
+}
   return(
+
     <div> 
        <Navbar />
        <Sidebar />
