8075f8559edcd046e5977fc37bd7f77dc04ea0af
rohitrodge <webrohit27@gmail.com>
2024-05-19 14:13:35 +0530

    Commit logo

--- study-hub/src/components/Navbar.js
+++ study-hub/src/components/Navbar.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Navbar = () => {
   return (
     <div>
-
+ 
+<Link to="/">
+    <img src={logo} />
+</Link>
     </div>
   )
 }
