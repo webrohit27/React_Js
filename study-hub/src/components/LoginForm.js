d12db7168e344407b0dd4730f49e36dc5041cd13
rohitrodge <webrohit27@gmail.com>
2024-05-22 21:29:51 +0530

    commit done

---
+++ study-hub/src/components/LoginForm.js
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+ import {AiOutLineEyeInvisible, AiOutLineEye} from "react-icons/ai"
+import { Link } from 'react-router-dom';
+
+const LoginForm = () => {
+
+const [formData, setFormData] = useState( {
+    email:'', password:""
+})
+
+const [showPassword, setShowPassword] = useState(false);
+
+function changeHandler(event){
+    
+    setFormData( (prevData) => (
+        {
+            ...prevData,
+            [event.target.name] : event.target.value
+        }
+    ))
+}
+
+  return (
+    <form>
+        <label>
+            <p>
+                Email Address <sup>*</sup>
+            </p>
+
+            <input
+            required 
+            type='text'
+            value={formData.email}
+            onChange={changeHandler}
+            placeholder='Enter email id'
+            />
+            </label>
+
+        <label>
+            <p>
+                Password<sup>*</sup>
+            </p>
+
+            <input
+            required 
+            type={showPassword ? ('text') : ("password")}
+            value={formData.password}
+            onChange={changeHandler}
+            placeholder='Enter Password'
+            />
+
+            <span onClick={()=> setShowPassword ((prev) => !prev)}>
+                {showPassword ? (<AiOutLineEyeInvisible />) : (<AiOutLineEye/> ) }
+            </span>
+
+            <Link>
+            to="#"
+            <p> 
+            forget password
+            </p>
+            </Link>
+        </label>
+         
+         <button>
+              Sign In
+        </button>
+        
+    </form>
+  )
+}
+
+export default LoginForm
--- study-hub/src/components/Template.js
+++ study-hub/src/components/Template.js
@@ -1,13 +1,55 @@
 import React from 'react'
+// import frameImage from "../assets/pic1.jpg"
 
-const Template = () => {
+const Template = ({title, desc1, desc2, image, formtype, setIsLoggedIn}) => {
+
   return (
     <div>
       
         <div>
-             
+             <h1>{title}</h1>
+             <p>
+              <span>{desc1}</span>
+              <span>{desc2}</span>
+             </p>  
+
+             {formtype === "signup" ?
+             (<SignupForm />):
+             (<LoginForm />)}
+
+             <div>
+              <div></div>
+              <p>OR</p>
+              <div></div>
+             </div>
+
+             <button>
+                Sign in With Google
+             </button>
         </div>
 
+
+        <div>
+          {/* <img src={frameImage}
+          alt="Pattern"
+          width={558}
+          height={504}
+          loading='lazy' /> */}
+
+          <img src={frameImage}
+          alt="Students"
+          width={558}
+          height={504}
+          loading='lazy' />
+
+          <img src={frameImage}
+          alt="Pattern"
+          width={558}
+          height={490}
+          loading='lazy' />
+
+
+        </div>
     </div>
   )
 }
