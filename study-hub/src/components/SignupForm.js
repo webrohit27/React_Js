de3f4a6d36cd2594f31f07ca68b04dd9ef67388c
rohitrodge <webrohit27@gmail.com>
2024-05-31 08:49:50 +0530

    commit done

--- study-hub/src/components/SignupForm.js
+++ study-hub/src/components/SignupForm.js
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Route, Routesd } from 'react-router-dom'
 
 const SignupForm = () => {
   return (
     <div>
         <h1>Please sign up Here</h1>
+        <Routes>
+          <Route></Route>
+        </Routes>
     </div>
   )
 }
